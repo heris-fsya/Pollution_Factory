6a5c40b8f555970afd80d20dc265835c8d742603
heris-fsya <fsya45@gmail.com>
2022-11-04 07:47:53 +0100

    map+case clicable +placement de batiment
    refait la map + case clicable + menu fonctionnelle + bâtiment qui se place + correction de bug + map généré

---
+++ Assets/script/CaseScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaseScript : MonoBehaviour
+{
+
+   
+
+    private int x { get; set; }
+    private int y { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
