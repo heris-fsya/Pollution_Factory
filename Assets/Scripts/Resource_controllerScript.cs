24ae1a82a1de6aa708a14f9218ff2d93422af636
jojo1209 <78849419+jojo1209@users.noreply.github.com>
2022-11-14 22:03:24 +0100

    add toute les mine et usine

--- Assets/Scripts/Resource_controllerScript.cs
+++ Assets/Scripts/Resource_controllerScript.cs
@@ -7,11 +7,262 @@ using UnityEngine.UIElements;
 
 
 
+
+public class Resource_controllerScript : MonoBehaviour
+{
+
+    #region variable génerale
+
+    
+    public int Polution;//polution génerale
+    
+    int maxPolution = 1000;//valeur de game over
+    
+    private int polutionT;//variable pour le passage de tour
+
+    public int Nturn;//le nombre de tour fait;
+
+    public int dollard /*{ get; private set; }*/;
+
+    #endregion
+
+
+
+
+
+
+
+    //public bool nextTurn;
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    #region variable ressource
+
+    private Ressource LithiumCobalt, Cuivre, Silicium, Neo;//stock ressource 
+    
+    public int LithiumCobaltT, CuivreT, SiliciumT, NeoT;//prod ressource (nom+T pour tour l'évolution de la ressource sur un tour)
+
+    #endregion
+
+    #region variable produit
+
+    public int phoneT, batteryT, aimantEolienneT;//prod produit (nom+T pour tour l'évolution du produit sur un tour)
+
+
+    //prix produit en $
+    private int phoneVal=600;
+    private int batteryVal=5000;
+    private int aimantEolienneVal=20000;
+
+    //prix produit ressource
+    
+    private Ressource[] phoneRes, batteryRes, aimantEolienneRes;
+
+    public Tuple<String, int>[] phoneCost = 
+        {   new Tuple<String, int>("LithiumCobalt", 5),
+            new Tuple<String, int>("Cuivre", 5), 
+            new Tuple<String, int>("Silicium", 0), 
+            new Tuple<String, int>("Neodyme",5)};
+
+    public Tuple<String, int>[] batteryCost =
+    {   new Tuple<String, int>("LithiumCobalt", 5),
+            new Tuple<String, int>("Cuivre", 5),
+            new Tuple<String, int>("Silicium", 5),
+            new Tuple<String, int>("Neodyme",0)};
+
+    public Tuple<String, int>[] aimantEolienneCost =
+    {   new Tuple<String, int>("LithiumCobalt", 5),
+            new Tuple<String, int>("Cuivre", 5),
+            new Tuple<String, int>("Silicium", 0),
+            new Tuple<String, int>("Neodyme",5)};
+
+
+
+
+    #endregion
+
+
+
+
+    #region variable batiment
+
+    enum nom
+    {
+        Vide = -1,
+        Foret,//arbre
+        MineLithiumCobalt,
+        MineCuivre,
+        MineSilicium,
+        MineNeodyme,
+        UsinePhone,
+        UsineEolienne,
+        UsineCar,
+        Recyclage,
+        silo,
+        Mine2,// le meme que le 1 en model 3d
+        UsinePhone2,
+        UsineEolienne2,
+        UsineCar2,
+        //Recyclage2,
+        silo2,
+        sapin,
+        rocher,
+    }
+
+
+    public int polarbre = -1;
+    public int polmine = +1;
+    public int polusine = +1;
+    public int polrecyclage = -1;
+    public int siloStockagevalue = +100;
+
+    public int maxStockage = 100;
+
+
+    //nombre de batiment 
+    public int arbre;
+    public int MineLithiumCobalt;
+    public int MineCuivre;
+    public int MineSilicium;
+    public int MineNeodyme;
+    public int UsinePhone;
+    public int UsineEolienne;
+    public int UsineCar;
+    public int recyclage;
+    public int silo;
+
+    /*//se qui reste a faire
+    Mine2
+    UsinePhone2
+    UsineEolienne2
+    UsineCar2
+    silo2
+    sapin
+    rocher*/
+
+    #endregion
+
+
+
+    #region fonctio génerale
+
+    void Start()
+    {
+        //nextTurn = false;
+        
+        LithiumCobalt=new Ressource("LithiumCobalt",100, 0);
+        Cuivre= new Ressource("Cuivre", 100, 0);
+        Silicium = new Ressource("Silicium", 100, 0);
+        Neo = new Ressource("Neodyme", 100, 0);
+
+        
+        phoneRes = new Ressource[] 
+        { LithiumCobalt.clonageQ(6), Cuivre.clonageQ(6) };
+
+        batteryRes= new Ressource[] 
+        { LithiumCobalt.clonageQ(15), Cuivre.clonageQ(15), Silicium.clonageQ(15) };
+
+        aimantEolienneRes = new Ressource[] 
+        { Cuivre.clonageQ(60), Neo.clonageQ(60) };
+
+    }
+
+    
+    void Update()
+    {
+       // if (nextTurn) { turn(); }
+    }
+
+    
+    
+
+    void turn()// passage du tour
+    {
+        //nextTurn = false;
+
+
+        LithiumCobalt.turn(LithiumCobaltT,maxStockage);
+        Cuivre.turn(CuivreT, maxStockage);
+        Silicium.turn(SiliciumT, maxStockage);
+        Neo.turn(NeoT, maxStockage);
+
+        dollard+=phoneT* phoneVal;
+        dollard+=batteryT* batteryVal;
+        dollard+=aimantEolienneT* aimantEolienneVal;
+
+
+
+        if (Nturn % 2 == 0) { polutionT += arbre * polarbre; }
+
+        Polution += polutionT;
+        Nturn += 1;
+    }
+
+    #endregion
+
+    #region fontion batiment
+
+    public void addSilo() { silo++;maxStockage += siloStockagevalue; }
+    public void removeSilo() { silo--; maxStockage -= siloStockagevalue; }
+
+    public void addArbre() { arbre++; }
+    public void removeArbre() { arbre--; }
+
+    #region fontion batiment Mine
+
+    public void addMineLithiumCobalt() { MineLithiumCobalt++; }
+    public void removeMineLithiumCobalt() { MineLithiumCobalt--; }
+
+    public void addMineCuivre() { MineCuivre++; }
+    public void removeMineCuivre() { MineCuivre--; }
+
+    public void addMineSilicium() { MineSilicium++; }
+    public void removeMineSilicium() { MineSilicium--; }
+
+    public void addMineNeodyme() { MineNeodyme++; }
+    public void removeMineNeodyme() { MineNeodyme--; }
+
+    #endregion
+
+
+
+    #region fontion batiment Usine
+    public void addUsinePhone() { UsinePhone++;  }
+    public void removeUsinePhone() { UsinePhone--;  }
+    public void addUsineEolienne() { UsineEolienne++; }
+    public void removeUsineEolienne() { UsineEolienne--; }
+    public void addUsineCar() { UsineCar++; }
+    public void removeUsineCar() { UsineCar--; }
+    #endregion
+
+
+    public void addRecyclage() { recyclage++; }
+    public void removeRecyclage() { recyclage--;  }
+
+    #endregion
+
+
+
+}
+
+
+
+
 public class Ressource
 {
-   private string nom;
-   private int valeur;
-   private int quantite;
+    private string nom;
+    private int valeur;
+    private int quantite;
 
 
     public Ressource(string n, int v, int q)
@@ -37,10 +288,11 @@ public class Ressource
 
     public bool plusGrandQue(Ressource n) { return n.getQuantite() < quantite; }
 
-    public bool memeNomQue(Ressource n) { return string.Equals(n.getName(), nom);  }
+    public bool memeNomQue(Ressource n) { return string.Equals(n.getName(), nom); }
 
     public Ressource clonageQ(int n) // la meme ressource dans des quantitée dif
-    { Ressource t= new Ressource(this);
+    {
+        Ressource t = new Ressource(this);
         t.setQuantite(n);
         return t;
     }
@@ -48,175 +300,3 @@ public class Ressource
 
 }
 
-public class Resource_controllerScript : MonoBehaviour
-{
-    public bool nextTurn;
-
-
-
-    //[SerializeField]
-    public int Polution /*{ get; private set; }*/;
-    int maxPolution = 1000;
-    
-    public int Nturn;//le nombre de tour fait;
-
-    
-
-    public int dollard /*{ get; private set; }*/;
-
-    public int polarbre = -1;
-    public int polmine = +1;
-    public int polusine = +1;
-    public int polrecyclage = -1;
-    public int siloStockagevalue = +100;
-
-    public int maxStockage=-100;
-
-    //stock ressource 
-    
-
-    
-
-
-    private Ressource LithiumCobalt, Cuivre, Silicium, Neo;
-
-
-    //prod ressource (nom+T pour tour l'évolution de la ressource sur un tour)
-    public int LithiumCobaltT, CuivreT, SiliciumT, NeoT;
-
-
-    //prod produit (nom+T pour tour l'évolution du produit sur un tour)
-    public int phoneT;
-    public int batteryT;//voiture
-    public int aimantEolienneT;
-
-    
-
-    //prix produit en $
-    private int phoneVal=600;
-    private int batteryVal=5000;
-    private int aimantEolienneVal=20000;
-
-    //prix produit ressource
-    
-    private Ressource[] phoneRes ;
-    private Ressource[] batteryRes  ;
-    private Ressource[] aimantEolienneRes  ;
-    
-    
-
-
-    //variable pour le passage de tour
-    private int polutionT;
-
-
-    enum nom// liste des batiment pour référence 
-    {
-        Vide = -1,
-        Foret,//arbre
-        Mine,
-        UsinePhone,
-        UsineEolienne,
-        UsineCar,
-        Recyclage,
-        silo,
-        Mine2,// le meme que le 1 en model 3d
-        UsinePhone2,
-        UsineEolienne2,
-        UsineCar2,
-        //Recyclage2,
-        silo2,
-        sapin,
-        rocher,
-    }
-
-    //nombre de batiment 
-    public int arbre;
-    public int mine;
-    public int usine;
-    public int silo;
-    public int recyclage;
-
-
-    void Start()
-    {
-        nextTurn = false;
-        
-        LithiumCobalt=new Ressource("LithiumCobalt",100, 0);
-        Cuivre= new Ressource("Cuivre", 100, 0);
-        Silicium = new Ressource("Silicium", 100, 0);
-        Neo = new Ressource("Neodyme", 100, 0);
-
-        
-        phoneRes = new Ressource[] 
-        { LithiumCobalt.clonageQ(6), Cuivre.clonageQ(6) };
-
-        batteryRes= new Ressource[] 
-        { LithiumCobalt.clonageQ(15), Cuivre.clonageQ(15), Silicium.clonageQ(15) };
-
-        aimantEolienneRes = new Ressource[] 
-        { Cuivre.clonageQ(60), Neo.clonageQ(60) };
-
-    }
-
-    
-    void Update()
-    {
-        if (nextTurn) { turn(); }
-    }
-
-    
-    public float polutionBarre() 
-    {
-        float p = Polution;
-        p /= maxPolution;
-        return p;
-        
-    }
-
-    void turn()// passage du tour
-    {
-        nextTurn = false;
-
-
-        LithiumCobalt.turn(LithiumCobaltT,maxStockage);
-        Cuivre.turn(CuivreT, maxStockage);
-        Silicium.turn(SiliciumT, maxStockage);
-        Neo.turn(NeoT, maxStockage);
-
-        dollard+=phoneT* phoneVal;
-        dollard+=batteryT* batteryVal;
-        dollard+=aimantEolienneT* aimantEolienneVal;
-
-
-
-        if (Nturn % 2 == 0) { polutionT += arbre * polarbre; }
-
-        Polution += polutionT;
-        Nturn += 1;
-    }
-
-    #region fontion batiment
-
-    
-
-    public void addSilo() { silo++;maxStockage += siloStockagevalue; }
-    public void removeSilo() { silo--; maxStockage -= siloStockagevalue; }
-
-    public void addArbre() { arbre++; }
-    public void removeArbre() { arbre--; }
-
-    public void addMine() { mine++; }
-    public void removeMine() { mine--; }
-
-    public void addUsine() { usine++;  }
-    public void removeUsine() { usine--;  }
-
-    public void addRecyclage() { recyclage++; }
-    public void removeRecyclage() { recyclage--;  }
-
-    #endregion
-
-
-
-}
--- Assets/Scripts/casse.cs
+++ Assets/Scripts/casse.cs
@@ -14,26 +14,7 @@ public class casse : MonoBehaviour
     private GameObject building=null;
     //public ligneScript ligneScript;
     public Resource_controllerScript ressource;
-    enum nom
-    {
-        Vide = -1,
-        Foret,//arbre
-        Mine,
-        UsinePhone,
-        UsineEolienne,
-        UsineCar,
-        Recyclage,
-        silo,
-        Mine2,
-        UsinePhone2,
-        UsineEolienne2,
-        UsineCar2,
-        //Recyclage2,
-        silo2,
-        sapin,
-        rocher,
-
-    }
+    
 
     public bool one = true;
 
@@ -56,52 +37,82 @@ public class casse : MonoBehaviour
         
     }
 
-    void turn()
+
+
+
+
+    enum nom
     {
-
-        
-
+        Vide = -1,
+        Foret,//arbre
+        MineLithiumCobalt,
+        MineCuivre,
+        MineSilicium,
+        MineNeodyme,
+        UsinePhone,
+        UsineEolienne,
+        UsineCar,
+        Recyclage,
+        silo,
+        Mine2,// le meme que le 1 en model 3d
+        UsinePhone2,
+        UsineEolienne2,
+        UsineCar2,
+        //Recyclage2,
+        silo2,
+        sapin,
+        rocher,
     }
 
-    
 
-
-
-    
-   public void creebat(int n)
+    public void creebat(int n)
     {
         //supresion batiment si existe
         switch (build)
         {
-            case 0: ; break;
-            case 1: ; break;
-            case 2: ; break;
-            case 3: ; break;
-            default:break;
+            case 0: ressource.removeArbre(); break;
+            case 1: ressource.removeMineLithiumCobalt(); break;
+            case 2: ressource.removeMineCuivre(); break;
+            case 3: ressource.removeMineNeodyme(); break;
+            case 4: ressource.removeMineLithiumCobalt(); break;
+            case 5: ressource.removeUsinePhone(); break;
+            case 6: ressource.removeUsineEolienne(); break;
+            case 7: ressource.removeUsineCar(); break;
+            case 8: ressource.removeRecyclage(); break;
+            case 9: ressource.removeSilo(); break;
+            default: break;
         }
 
         //ajout batiment
-        build = n;
-        switch (build)
-        {
-            case 0: ressource.addArbre(); break;
-            case 1: ressource.addMine(); break;
-            case 2: ressource.addUsine(); break;
-            case 3: ressource.addRecyclage(); break;
-            default: break;
-        }
+        
         //
-        if (building.IsUnityNull())
+        if (build==-1)
         {
             
             building = Instantiate(batiment[n], possition, rotation, transform);
         }
         else 
-        { 
-
-            Destroy(building);
+        {
+            building.active=false;
+            //Destroy(building);
             building =Instantiate(batiment[n], possition, rotation, transform);
             
+        }
+
+        build = n;
+        switch (build)
+        {
+            case 0: ressource.addArbre(); break;
+            case 1: ressource.addMineLithiumCobalt(); break;
+            case 2: ressource.addMineCuivre(); break;
+            case 3: ressource.addMineNeodyme(); break;
+            case 4: ressource.addMineLithiumCobalt(); break;
+            case 5: ressource.addUsinePhone(); break;
+            case 6: ressource.addUsineEolienne(); break;
+            case 7: ressource.addUsineCar(); break;
+            case 8: ressource.addRecyclage(); break;
+            case 9: ressource.addSilo(); break;
+            default: break;
         }
     }
 }
